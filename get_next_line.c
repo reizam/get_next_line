dbe5ea7b34a1eb0749949eae7e42cc90f4a95ca1
kmazier <kmazier@student.42.fr>
2020-11-23 01:31:13 +0100

    correct

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kmazier <kmazier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 17:33:43 by kmazier           #+#    #+#             */
-/*   Updated: 2020/11/23 00:55:36 by kmazier          ###   ########.fr       */
+/*   Updated: 2020/11/23 01:22:47 by kmazier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,16 @@ int		get_next_line_offset(t_list *lst, ssize_t ret)
 	return (ret == 0 ? i : 0);
 }
 
-int		parse_line(size_t v, size_t end, char *content, char **line)
+int		parse_line(size_t start, size_t end, char *content, char **line)
 {
 	size_t	i;
 	char	*result;
 
 	i = 0;
-	if (!(result = (char*)malloc(sizeof(char) * ((v ? end : 0) + 1))))
+	if (!(result = (char*)malloc(sizeof(char) * ((end - start) + 1))))
 		return (0);
-	while (!v && i < end)
-	{
-		result[i] = content[i];
-		i++;
-	}
+	while (start < end)
+		result[i++] = content[start++];
 	result[i] = 0;
 	*line = result;
 	return (1);
@@ -80,7 +77,7 @@ int		get_next_line(int fd, char **line)
 			temp->content = ft_strjoin(temp->content, buffer, i);
 	if (i < 0)
 		return (-1);
-	parse_line(i == 0 && j == 0, i == 0 && j > 0 ? ft_strlen(temp->content) : j - 1, temp->content, line);
+	parse_line(0, i == 0 ? ft_strlen(temp->content) : j - 1, temp->content, line);
 	free_content(&temp, j);
 	return (i == 0 && j == 0 ? 0 : 1);
 }
